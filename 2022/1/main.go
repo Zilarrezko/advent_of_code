1d8d517ead276a7f08c46608a85f07ae41c77364
Zilarrezko <jesse@Zilarrezko.dev>
2023-01-17 23:30:40 -0800

    2022.d01.p1 simplify

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -15,19 +15,15 @@ func main() {
         log.Fatal(err);
     }
 
-    var max_calorie_elf int;
     var max_calories int;
-    var elf int = 1;
     var calories int;
     lines := strings.Split(string(data), "\n");
     for _, line := range lines {
         if line == "" {
             if calories > max_calories {
                 max_calories = calories;
-                max_calorie_elf = elf;
             }
             calories = 0;
-            elf += 1;
         } else {
             calorie, err := strconv.Atoi(line);
             if err != nil {
@@ -36,5 +32,5 @@ func main() {
             calories += calorie;
         }
     }
-    fmt.Println("elf:", max_calorie_elf, "calories:", max_calories);
+    fmt.Println("max calories:", max_calories);
 }
