061ae242eb4228aeb72115e41beba9f941029b5f
Zilarrezko <jesse@Zilarrezko.dev>
2023-01-18 23:29:00 -0800

    2022.d06.p2

--- 2022/6/main.go
+++ 2022/6/main.go
@@ -7,15 +7,17 @@ import (
     "log"
 )
 
+const g_window_size int = 14;
+
 func main () {
     data, err := os.ReadFile("input.txt");
     if err != nil {
         log.Fatal(err);
     }
-    for i := 0; i < len(data) - 3; i += 1 {
-        window := data[i:i+4];
+    for i := 0; i < len(data) - g_window_size - 1; i += 1 {
+        window := data[i : i + g_window_size];
         if !has_duplicate(window) {
-            fmt.Println(i + 4);
+            fmt.Println(i + g_window_size);
             break;
         }
     }
